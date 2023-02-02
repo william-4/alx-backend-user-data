131c9d0d809d69a5910f306cbabb32fd6b13365b
william-4 <mwangiiwilliam@gmail.com>
2023-02-02 06:18:01 +0300

    task 0

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Main module
+"""
+
+
+import re
+
+
+def filter_datuem(fields: [str], redaction: str, message: str,
+                  seperator: str) -> str:
+    """
+    Returns an obfuscated
+    Args:
+        fields (list): strings representing all fields to obfuscate
+        redaction (str): what the field will be obfuscated
+        message (str): the log line
+        seperator (str): character seperating all fields in the log line
+    """
+    for field in fields:
+        message = re.sub(field+'=.*?'+separator,
+                         field+'='+redaction+separator, message)
+    return message
