367f8de015350ff10397fbf30bf1e8919071774b
william-4 <mwangiiwilliam@gmail.com>
2023-02-08 05:50:41 +0300

    task 2

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -34,4 +34,14 @@ def unauthorized() -> str:
       - raise a 401 error by using abort
     """
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """
+    GET /api/v1/forbidden
+    Return:
+      - raise a 403 error by using abort
+    """
+    abort(403)
     
