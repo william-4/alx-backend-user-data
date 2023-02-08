9a6f9b9e9a6e1dd3231393247ed2644291b87b88
william-4 <mwangiiwilliam@gmail.com>
2023-02-08 05:54:05 +0300

    pycodestyle

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -36,6 +36,7 @@ def forbidden(error) -> str:
     """
     return jsonify({"error": "forbidden"}), 403
 
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
