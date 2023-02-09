834cd89c2283e8b9779e45f3a24fa4d7daa23127
william-4 <mwangiiwilliam@gmail.com>
2023-02-09 06:05:13 +0300

    task 4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,23 +12,26 @@ class Auth():
     Class to manage the API authentication
     """
 
-    def __init__(self):
-        """
-        Method called during initialization
-        """
-        pass
-
     def require_auth(self, path: str, excluded_paths: list[str]) -> bool:
         """
         Method that returns false
         """
-        return (False)
+        if path is None:
+            return (True)
+        elif excluded_paths is None or excluded_paths == []:
+            return (True)
+        elif path in excluded_paths or path + '/' in excluded_paths:
+            return (False)
+        else:
+            return (True)
 
     def authorization_header(self, request=None) -> TypeVar('User'):
         """
         Method that returns None
         """
-        return (None)
+        if request is None:
+            return (None)
+        
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
