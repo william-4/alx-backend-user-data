722c82bce7df0289c3c8e1a8322001962037ed1e
william-4 <mwangiiwilliam@gmail.com>
2023-02-09 05:37:56 +0300

    task 3

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+Module to manage API authentication
+"""
+
+from flask import request
+
+
+class Auth():
+    """
+    Class to manage the API authentication
+    """
+
+    def __init__():
+        """
+        Method called during initialization
+        """
+        pass
+
+    def require_auth(self, path: str, excluded_paths: list[str]) -> bool:
+        """
+        Method require_auth
+        Returns:
+            - false
+        """
+        return (False)
+
+    def authorization_header(self, request=None) -> TypeVar('User'):
+        """
+        Returns None
+        """
+        return (None)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Returns None
+        """
