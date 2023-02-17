07d2ee9b019a4caaed75e2a60f5259039c15a1f3
william-4 <mwangiiwilliam@gmail.com>
2023-02-17 06:08:11 +0300

    task 0-3

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,21 @@
+#!/user/bin/env python3
+"""Module defining a user class in our database"""
+
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    """Table User with the attributes"""
+    __tablename__ = 'users'
+
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
